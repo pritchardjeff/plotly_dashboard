750132a9ff6139ebee4f35007369883a750393e9
pritchardjeff <68468033+pritchardjeff@users.noreply.github.com>
2020-10-11 15:59:21 -0400

    format

--- charts.js
+++ charts.js
@@ -72,7 +72,7 @@ function buildCharts(sample) {
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
-    var otuSort = otu_ids.slice(0,10).sort((a,b) => b - a);
+    var otuSort = otu_ids.slice(0,10).sort((a,b) => a - b);
     var yticks = otuSort.map(otu => "OTU " + otu)
 
     // 8. Create the trace for the bar chart. 
